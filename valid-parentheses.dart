9583ec7daa55013c2d4a5da337e026aaddf829e2
Xushnudbek Abdusamatov <xuwnudbek266@gmail.com>
2025-11-13 10:12:32 +0500

    1d3t9brc298713

--- valid-parentheses.dart
+++ valid-parentheses.dart
@@ -1,1 +1,19 @@
+void main() {
+  final Solution solution = Solution();
 
+  var res = solution.isValid("({}[])");
+
+  print("isValid: $res");
+}
+
+class Solution {
+  bool isValid(String s) {
+    var match = RegExp(r"\(\)|\[\]|\{\}");
+
+    while (match.hasMatch(s)) {
+      s = s.replaceAll(match.firstMatch(s)!.group(0)!, "");
+    }
+
+    return s.isEmpty;
+  }
+}
